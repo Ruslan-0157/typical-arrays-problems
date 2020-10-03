862e3af14089e9ee40b169a93e454940146233fe
Rahimov Ruslan <dragon-2200@mail.ru>
2020-10-03 21:59:08 +0500

    feat: add three functions max, min, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    if (!array) return 0;
+    if (array.length === 0) return 0;
+    let minimal = 0;
+    array.map((nextNumber, index, array) => {
+        minimal = index === 0 ? nextNumber : minimal;
+        if (index < array.length && minimal > nextNumber) {
+            minimal = nextNumber;
+        }
+    });
+    return minimal;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array) return 0;
+    if (array.length === 0) return 0;
+    let maximal = 0;
+    array.map((nextNumber, index, array) => {
+        maximal = index === 0 ? nextNumber : maximal;
+        if (index < array.length && maximal < nextNumber) {
+            maximal = nextNumber;
+        }
+    });
+    return maximal;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array) return 0;
+    if (array.length === 0) return 0;
+    const length = array.length;
+    let sum = 0;
+    array.map((nextNumber) => {
+        sum += nextNumber;
+    });
+    const avg = sum / length;
+    return avg;
+};
